6fb5306e0f305d1cd22de5d190404e82cd497056
Seju <sbbidiwala@gmail.com>
2017-04-25 19:58:31 -0500

    Renamed to 'Game-azon', encoded string for bgg search, insert rating in green under product title instead of alerting.

--- content.js
+++ content.js
@@ -8,7 +8,15 @@ function httpGet(theUrl)
       if (xhr.status === 200) {
         var ratings = xhr.responseText.match(/collection_bggrating\D*([0-9]{1,2}\.[0-9]{1,3})/);
         if (ratings) {
-          alert('Found rating on BGG: ' + ratings[1]);
+          var titleSpan = $('#productTitle')[0];
+
+          var el = document.createElement('div');
+
+          el.innerHTML = 'BGG rating: ' + ratings[1];
+          el.style['color'] = 'green';
+          el.style['font-size'] = '16px';
+
+          titleSpan.append(el);
         } else {
           console.log('No results found');
         }
@@ -39,6 +47,10 @@ if (category != 'Toys & Games') {
   title = title.replace(/edition/gi, '');
   title = title.replace(/\d+st|\d+nd|\d+rd|\dth/gi, '');
   title = title.replace(/expansion/gi, '');
+  title = title.replace(/\s/gi, '+');
+  title = title.replace(/&/gi, '');
+  title = title.trim();
+  title = encodeURI(title);
 
   console.log('Looking for title: ' + title);
   var bggResponse = httpGet('https://boardgamegeek.com/geeksearch.php?action=search&objecttype=boardgame&q=' + title);
